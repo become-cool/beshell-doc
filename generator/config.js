2f30b732eee81c2e498a41f62fa599baf27ace1a
alee <alee@chou.im>
2024-05-31 19:51:46 +0800

    x

--- generator/config.js
+++ generator/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    path_beshell: __dirname + "/../../beshell-arduino-build/libraries-src/BeShell" ,
+    path_beshell: __dirname + "/../../beshell-build/libraries-src/BeShell" ,
     path_lv_generator: __dirname + "/../../beshell-lvgl-generator",
     
     path_output: __dirname + "/../src/"  
